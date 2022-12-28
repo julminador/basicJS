c1c04444e11efc195e9b63c0ad91b5e3058d25f9
julminador <juliansosa021@gmail.com>
2022-12-28 15:20:09 -0500

    Closures and parctice exam

--- src/clousure.js
+++ src/clousure.js
@@ -1,22 +1,58 @@
 // Las clousures recuerdadn las variabales a las que podian acceder por mas que se las invoque desde otro lugar
-const myGlobal = 0;
-
-function myFunction() {
-  const myNumber = 1;
-  console.log(`%c Error: ${myGlobal} `, 'background:black; color: red;');
-
-  function parent() {
-    const inner = 2;
-    console.log(myNumber, myGlobal);
-
-    function child() {
-      console.log(inner, myNumber, myGlobal);
-    }
-
-    return child();
+function crearContador(contador = 0) {
+  return {
+    incrementar: function() {
+      return contador++;
+    },
+    decrementar: function() {
+      return contador--;
+    },
+    obtenerValor: function() {
+      return contador;
+    },
   }
-
-  return parent();
 }
 
-myFunction();
+const contador1 = crearContador(10);
+contador1.incrementar();
+contador1.incrementar();
+contador1.incrementar();
+contador1.decrementar();
+console.log(contador1.obtenerValor());
+
+const contador2 = crearContador();
+contador2.incrementar();
+console.log(contador2.obtenerValor());
+
+
+
+// tambien es un buena opcion para crear funciones dinamicas
+const estilosPorDefecto = 'color: white;'; // esta variable se declara en el entorno lexito externo para ahorrar memoria
+function crearImpresoraDeMensajes(tipo, estilos) {
+  return function mensaje(str) {
+    console.log(`%c ${tipo}: ${str} `, estilosPorDefecto + estilos);
+  }
+}
+
+const error = crearImpresoraDeMensajes('Error', 'background: red;');
+const warning = crearImpresoraDeMensajes('Warning', 'background: orange;');
+const exito = crearImpresoraDeMensajes('Exito', 'background: green;');
+
+warning('Esta seguro de continuar?');
+
+
+// button.addEventListener('click', onClick);
+// ...
+// button.removeEventListener('click', onClick);
+
+
+function sumWithClosure(firstNum) {
+  return function (secondNum) {
+    if (!secondNum) return firstNum;
+    else return firstNum + secondNum;
+  }
+}
+
+// forma de ejecutar con dos parametros
+console.log( sumWithClosure(2)(3) ); // 5
+console.log( sumWithClosure(90)() ); // 90
