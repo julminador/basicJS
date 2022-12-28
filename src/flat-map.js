27d6ae5b70e246230f0126c4cb0851815929e667
julminador <juliansosa021@gmail.com>
2022-12-28 16:26:12 -0500

    flat-map code fix

--- src/flat-map.js
+++ src/flat-map.js
@@ -3,5 +3,5 @@ const array = [ 1, 2 ,3, 4, [1, 3, 5, 6, ,[1, 2, 4]]];
 console.log(array.flat(2)); // flat puede "aplanar una matriz de acuerdo con la profundidad"
 
 //flatmap
-const array2 = [ 1, 2 ,3, 4, 5];
-console.log(array.flatMap(v => [v, v * 2]));
+const array2 = [ 1, 2, 3, 4, 5];
+console.log(array2.flatMap(v => [v, v * 2]));
